fd7adcf7ff78635466ffbdd67977db9edffb0a48
Dini-is-a-dinasour <DDeSilva@smail.pembinatrails.ca>
2024-10-25 16:26:17 -0500

    changed the format a bit,
    hopefully that does the trick

--- Gitrepo.sh
+++ Gitrepo.sh
@@ -1,19 +1,19 @@
 #!bin/bash
 echo "do you want to hear a joke? (yes or no)"
 read user
-if [ "$user" == "yes" ];then
+if [ "$user" == "yes" ]; then
   echo "randomizing output........"
-sleep && 5 "finished"
-sentences=(
+ sleep && 5 "finished"
+ sentences=(
     "write joke here"
     "write joke here"
     "write joke here"
     "write joke here"
     "write joke here"
-)
-RANDOM=$[RANDOM_t % ${#sentences(@)}]
-echo "${sentences[$RANDOM_t]}"
+ )
+ RANDOM=$[RANDOM_t % ${#sentences(@)}]
+ echo "${sentences[$RANDOM_t]}"
 
-elsif [ "$user" == "no"];then
+else [ "$user" == "no"]; then
   echo "awwwwwwww :("
 fi
