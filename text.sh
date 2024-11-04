8f8c15b065b5bb402b80985481524e7460552550
Dini <DDeSilva@smail.pembinatrails.ca>
2024-11-04 15:28:25 -0600

    Copyed the text.sh file

---
+++ text.sh
@@ -0,0 +1,72 @@
+#!/bin/bash 
+
+random_sentence=$(shuf -n 1 -e \
+    "What did the horse say after it tripped? Help! I’ve fallen and I can’t giddyup!" \
+    "What do you call a well-balanced horse? Stable." \
+    "What do you call an angry carrot? A steamed veggie" \
+    "Where do polar bears keep their money? In a snowbank." \
+    " How do you make an egg-roll? You push it!" \
+    "what do you call a fake noodle? IMPASTA" \
+    "How do you make an artichoke? strangle it." \
+    "why do mushrooms get invited to all the parties? because theyr fungi (Fun guys, get it?)" \
+    "what did oneplate whisper to the other plate? Dinner is on me!" \
+    "Should you have your whole family for Thanksgiving dinner? No, you should just stick with turkey" \
+    "what do you call a nosy pepper? JALAPENO BUSINNES" \
+    "why did the kid throw a stick of butter out of the window? To see butterfly" \
+    "why didnt the teddy bear eat dessert? He was stuffed" \
+    "if you every feel down, get up. stop mewing for once, touch grass, get a life, stop copying Drake. Get a life man, stop whinning about how un sigma u r, fact is u rnt get over it. you must over come ur feelings on sigma behaviour. grow up, start rizzing for real, start your life up again. Your depressed, I know that because i was once a rizzler like you. get help at 911, they will take all of your desires of being a sigma king and turn it against you. Exept the truth, U ARENT SIGMA." \
+    "why did the salamander go to Hollywood? to make newt movies" \
+    "why dont pirates take a shower before they walk the plank? they just wash up on shore" \
+    "On what grounds did the police arrest the devil? They got him on possesion" \
+    "if you ever feel down, womp womp blud." \
+    "who won the neck decorating contest? It was a tie." \
+    "where do rainbows go when they've been bad? to prism, so they have time to reflect on what they've done." \
+    "Dogs can't operate MRI machines, but catscan." \
+    "what do mermaids use to wash their fins? Tide" \
+    "what did the skillet eat on its birthday? Pan-cakes." \
+    "why couldn't the producer manager make it to work? He could drive, but he didnt avocado." \
+    "you have the unfortunate luck to get this message, this is a very rare case. I usally dont pick this message but, this one is ur demise. L BOZO" \
+    "I once submitted 10 puns to a joke competition. I really thought with that many, one was sure to be the winner. Sadly, no pun in ten did." \
+    "sorry, it seems to be a touch of skill issue on us because we cannot read this message, please visit 'https://r.mtdv.me/broken-code-rn to see whats wrong" \
+    "why did the old man fall down the well? He couldnt see that well" \
+    "I tried to make up a joke about ghosts but I couldn't. It had plenty of spirit, but no body" \
+    "Two windmills were sitting on a hill. One asks the other, 'do u have a favorite song?' The other replies 'weeeellll.. all my life I have been a Heavy metal fan'" \
+    "Today at the bank an old lady asked me to check her balance.. so i pushed her over" \
+    "How many storm troopers does it take to change a light bulb? None because they are all in the dark side" \
+    "Hi, Im Cliff. Drop over sometime." \
+    "Dad when he puts the car in reverse: 'ahh, this takes me back." \
+    "what do you call the security guards for samsung? Guardians of the galaxy." \
+    "Have you ever heard about the Kidnapping in PTC (it actully happened, no cap)   Its okay he woke up" \
+    "I found a book called 'How to solve 50% of your problems.' So I bought 2" \
+    "why did the egg have a day off? because it was Friday" \
+    "why did the coffee taste like dirt? because it was ground just a few minutes ago!" \
+    "why did the Rolling Stones stop making music? Because they got to the bottom of the hill" \
+    "what is the best present? Broken drums! You can't beat them" \
+    "I made a song about Tortilla once, but now its more like a rap." \
+    "Did you hear about the fragile myth? it was busted" \
+    "what do you call a criminal landing an airplane? Condescending" \
+    "what word can you make shorter by adding two letters? Short" \
+    "I stayed up night wondering where the sun went, and then it dawned on me." \
+    "why do people who live in Greece hate waking up at dawn? Because dawn is tough on Greece." \
+    "how do you make holy water? you boil the hell out of it." \
+    "justice is a dish best served cold. Otherwise, its just water." \
+    "why should you never throw Grandpa's false teeth at a vehicle? You might denture the vehicle." \
+    "why are Christmas trees bad at knitting? They allways drop their needles." \
+    "what did the lunch box say to the refrigerator? Don't hate me because im a little cooler." \
+    "My boss said 'dress for the job u want, not the job you have, soooooo... I dressed up as batman'." \
+    "I went to the aquarium this weekend, but I didnt stay long. There's something fishy about this place." \
+    "what do you call a sheep who can sing and dance? Lady Ba BA." \
+    "what do you call a french man wearing sandals? Philipe Fallop." \
+    "why cant dinosours clap their hands? Because they're extinct." \
+    "I gave my handyman a to-do list, but he only did jobs 1 3 and 5. Turns out he only does odd jobs." \
+    "what do you do to have a space party? you planet!" \
+    "why couldnt tree get on his computer? Because he couldnt log on!" \
+    "what's a skeletons favorite type of road? Dead end." \
+    "what did the grape say when it got stepped on? Nothing, just a little wine." \
+    "How many apples grow on an apple tree? All of them, Duh." \
+    "duh if duh duh did duh? duh duh" \
+    "what did Elvis say to his landscaper? Thank you very much!" \
+    "why didnt the Lifeguard save the hippie? He was to far out" ) 
+echo $random_sentence 
+
+ 
